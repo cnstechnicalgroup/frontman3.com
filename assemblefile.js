58ed222990dc419875532363daf27d7110935b67
Sam Morrison <sam@cnstechgroup.com>
2015-07-19 10:11:28 +0900

    reorg

--- assemblefile.js
+++ assemblefile.js
@@ -1,15 +1,22 @@
 var assemble = require('assemble');
 var less = require('gulp-less');
+var extname = require("gulp-extname");
 
 assemble.task('html', function() {
   assemble.src('templates/*.hbs')
+    .pipe(extname())
     .pipe(assemble.dest('dist/'));
 });
 
-assemble.task('css', function () {
-  assemble.src('styles/*.less')
+/*assemble.task('css', function () {
+  assemble.src('styles/*.css')
     .pipe(less())
     .pipe(assemble.dest('dist/assets/css'));
+});*/
+
+assemble.task('assets', function () {
+  assemble.src('assets/*')
+    .pipe(assemble.dest('dist/assets/'));
 });
 
 assemble.task('default', ['html', 'css']);
